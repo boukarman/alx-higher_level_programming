1091669f0f462ac909e2696f07c94267f61193f4
boukarmann <boukarman@gmail.com>
2024-03-19 16:35:38 -0700

    2 args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-
-import sys
-
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == '__main__':
+    import sys
+    argv_len = len(sys.argv) - 1
+    if not argv_len == 1:
+        if not argv_len == 0:
+            argv_ = "arguments:"
+        else:
+            argv_ = "arguments."
+    else:
+        argv_ = "argument:"
+    argv_output = "{0:d} {1}".format(argv_len, argv_)
+    print(argv_output)
+    if argv_len > 0:
+        values = sys.argv[1:]
+        for index, value in enumerate(values, start=1):
+            value_ = "{0:d}: {1}".format(index, value)
+            print(value_)
