166869d7ca8b591112ad59a46df6c5a3c519d42a
boukarmann <boukarman@gmail.com>
2024-03-19 16:38:16 -0700

    2 args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    argv_length = len(sys.argv) - 1
-    if not argv_length == 1:
-        if not argv_length == 0:
-            argv_or_s = "arguments:"
+    argv_len = len(sys.argv) - 1
+    if not argv_len == 1:
+        if not argv_len == 0:
+            argv_ = "arguments:"
         else:
-            argv_or_s = "arguments."
+            argv_ = "arguments."
     else:
-        argv_or_s = "argument:"
-    argv_print = "{0:d} {1}".format(argv_length, argv_or_s)
-    print(argv_print)
-    if argv_length > 0:
+        argv_ = "argument:"
+    argv_output = "{0:d} {1}".format(argv_len, argv_)
+    print(argv_output)
+    if argv_len > 0:
         values = sys.argv[1:]
         for index, value in enumerate(values, start=1):
-            value_p = "{0:d}: {1}".format(index, value)
-            print(value_p)
+            value_ = "{0:d}: {1}".format(index, value)
+            print(value_)
