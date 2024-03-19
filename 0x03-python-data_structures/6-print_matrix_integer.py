abe6ada8417280b92a4c48a7d6dec8fdb1c2dbb8
boukarmann <boukarman@gmail.com>
2024-03-19 15:44:44 -0700

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for a in range(len(row)):
+            print("{:d}".format(row[a]), end=" " * (a < (len(row) - 1)))
+        print("")
